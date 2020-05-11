c710090b252c9f491e3eb11bde0bb23147570b8f
Kevin Escoto <kevin29_kare@hotmail.es>
2020-05-10 18:54:17 -0600

    :pencil: Add swagger documentation to logout

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -4,11 +4,12 @@ import { IAuthenticatedUser } from '../../users/interfaces/users.interface';
 import { AuthService } from '../services/auth.service';
 import { TokenResponse } from '../docs/token-response.doc';
 import { User } from '../../users/decorators/user.decorator';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { LoginDto } from '../dtos/login.dto';
 import { BearerToken } from '../decorators/bearer-token.decorator';
 import { SessionGuard } from '../guards/session.guard';
 
+@ApiTags('Auth Endpoints')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -22,6 +23,7 @@ export class AuthController {
 
   @UseGuards(SessionGuard)
   @HttpCode(204)
+  @ApiBearerAuth()
   @Delete('logout')
   logout(@BearerToken() accessToken: string): Promise<any> {
     return this.authService.logout(accessToken);
