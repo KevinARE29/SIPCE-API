fcf03d7abb69fe0a140de89dc656906a73932c7c
Kevin Escoto <kevin29_kare@hotmail.es>
2020-05-30 23:58:47 -0600

    :pencil: Update etl module

--- src/etl/etl.module.ts
+++ src/etl/etl.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { EtlController } from './etl.controller';
-import { EtlService } from './etl.service';
+import { AuthModule } from '@auth/auth.module';
+import { UsersModule } from '@users/users.module';
+import { EtlController } from './controllers/etl.controller';
+import { EtlService } from './services/etl.service';
 
 @Module({
+  imports: [AuthModule, UsersModule],
   controllers: [EtlController],
-  providers: [EtlService]
+  providers: [EtlService],
 })
 export class EtlModule {}
