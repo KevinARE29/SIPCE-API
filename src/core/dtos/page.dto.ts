82bc4617f6fb2afc753d8685d566657dd815ed4b
Kevin Escoto <kevin29_kare@hotmail.es>
2020-06-01 01:38:41 -0600

    :recycle: Refactor pageDto

--- src/core/dtos/page.dto.ts
+++ src/core/dtos/page.dto.ts
@@ -1,6 +1,7 @@
 import { IsPositive, IsInt, IsOptional, Max } from 'class-validator';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
+import { validator } from '@core/messages/validator.message';
 
 export class PageDto {
   @ApiPropertyOptional()
@@ -13,8 +14,8 @@ export class PageDto {
   @ApiPropertyOptional()
   @IsOptional()
   @Type(() => Number)
-  @IsInt()
-  @IsPositive()
-  @Max(25)
+  @IsInt({ message: validator.isInt })
+  @IsPositive({ message: validator.isPositive })
+  @Max(25, { message: validator.isMax })
   perPage = 10;
 }
