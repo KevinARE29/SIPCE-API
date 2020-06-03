da87de78d5ca1f96355a697208cf95774fddbddc
Kevin Escoto <kevin29_kare@hotmail.es>
2020-06-02 23:16:50 -0600

    :sparkles: Add etl service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { EtlModule } from './etl/etl.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ConfigModule.forRoot({ isGlobal: true }), UsersModule, AuthModule, EtlModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({ isGlobal: true }),
+    ScheduleModule.forRoot(),
+    UsersModule,
+    AuthModule,
+    EtlModule,
+  ],
 })
 export class AppModule {}
--- src/etl/services/etl.service.ts
+++ src/etl/services/etl.service.ts
@@ -4,23 +4,55 @@ import { ConfigService } from '@nestjs/config';
 import { IAuthenticatedUser } from '@users/interfaces/users.interface';
 import { createConnection } from 'mysql';
 import { Client } from 'pg';
-
+import { Cron } from '@nestjs/schedule';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const datapumps = require('datapumps');
 
 @Injectable()
 export class EtlService {
-  constructor(private readonly configService: ConfigService) {}
+  originUser!: string;
 
+  originPass!: string;
+
+  originHost!: string;
+
+  originDb!: string;
+
+  destUser!: string;
+
+  destPass!: string;
+
+  destHost!: string;
+
+  destDb!: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.originUser = configService.get('SNM_USERNAME') || '';
+    this.originPass = configService.get('SNM_PASSWORD') || '';
+    this.originHost = configService.get('SNM_HOST') || '';
+    this.originDb = configService.get('SNM_DATABASE') || '';
+    this.destUser = configService.get('TYPEORM_USERNAME') || '';
+    this.destPass = configService.get('TYPEORM_PASSWORD') || '';
+    this.destHost = configService.get('TYPEORM_HOST') || '';
+    this.destDb = configService.get('TYPEORM_DATABASE') || '';
+  }
+
+  // Second|Minute|Hour|Day of Month|Month|Day of Week
+  @Cron('30 30 12 15 * *')
   async syncStudents(user: IAuthenticatedUser): Promise<void> {
     const { Pump } = datapumps;
     const pump = new Pump();
-    // Extraction
-    const mysqlConnection = createConnection('mysql://sql9344499:ntdfgG62z4@sql9.freemysqlhosting.net/sql9344499')
+    // Extract
+    const mysqlConnection = createConnection(
+      `mysql://${this.originUser}:${this.originPass}@${this.originHost}/${this.originDb}`,
+    )
       .query('SELECT * from roles')
       .stream();
+
     const { PostgresqlMixin } = datapumps.mixin;
-    const postgresqlClient = new Client('postgresql://kevin:admin123@localhost/SipceDB');
+    const postgresqlClient = new Client(
+      `postgresql://${this.destUser}:${this.destPass}@${this.destHost}/${this.destDb}`,
+    );
     postgresqlClient.connect();
 
     pump
@@ -29,11 +61,14 @@ export class EtlService {
       .process(async (role: any) => {
         console.log('From Origin DB', role);
         await pump.query(`SELECT * FROM role WHERE id = ${role.role_id}`).then((res: any) => {
-          // Transformation
+          // Transform
           console.log('From Destination DB', res.rows[0]);
           // Load
           if (res.rowCount) {
-            pump.query(`UPDATE role SET name = '${role.nombre}' WHERE id = ${role.role_id}`);
+            if (role.nombre !== res.rows[0].name) {
+              pump.query(`UPDATE role SET name = '${role.nombre}' WHERE id = ${role.role_id}`);
+              console.log('UPDATE');
+            }
           } else {
             pump.query(`INSERT INTO role (id, name) VALUES (${role.role_id}, '${role.nombre}')`);
           }
