081e187234e40a2863eca26ff11566a62e135495
Kevin Escoto <kevin29_kare@hotmail.es>
2020-06-06 01:00:12 -0600

    :zap: Remove unnecesary variables

--- src/logs/interceptors/access-log.interceptor.ts
+++ src/logs/interceptors/access-log.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, Logger } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { AccessLogRepository } from '@logs/repositories/access-log.repository';
