6cf1b2862c22100111d00177df421dbce8a37bf6
Kevin Escoto <kevin29_kare@hotmail.es>
2020-06-05 23:01:48 -0600

    :pencil: Add access log migrations

---
+++ migrations/1591419199718-add-action-log-doc-migration.ts
@@ -0,0 +1,39 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Migration1591419199718 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(
+      `comment on table "action_log" is 'Tabla bitácora para almacenar los intentos de acceso al Sistema Informático';`,
+      undefined,
+    );
+    await queryRunner.query(
+      `comment on column "action_log".id is 'Id del registro en la bitácora de accesos';`,
+      undefined,
+    );
+    await queryRunner.query(
+      `comment on column "action_log".username is 'Nombre de usuario con el que se intentó iniciar sesión';`,
+      undefined,
+    );
+    await queryRunner.query(
+      `comment on column "action_log".ip is 'Dirección IP desde la cual intentó iniciar sesión';`,
+      undefined,
+    );
+    await queryRunner.query(
+      `comment on column "action_log".status_code is 'Código de la respuesta retornada por la API';`,
+      undefined,
+    );
+    await queryRunner.query(
+      `comment on column "action_log".attempt_time is 'Fecha y hora en la cual se intentó iniciar sesión';`,
+      undefined,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.query(`comment on table "action_log" is '';`, undefined);
+    await queryRunner.query(`comment on column "action_log".id is '';`, undefined);
+    await queryRunner.query(`comment on column "action_log".username is '';`, undefined);
+    await queryRunner.query(`comment on column "action_log".ip is '';`, undefined);
+    await queryRunner.query(`comment on column "action_log".status_code is '';`, undefined);
+    await queryRunner.query(`comment on column "action_log".attempt_time is '';`, undefined);
+  }
+}
