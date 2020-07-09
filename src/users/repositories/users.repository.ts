fe16f94da1cc174939cf59b4b93219f545be8d2e
Kevin Escoto <kevin29_kare@hotmail.es>
2020-07-09 02:19:59 -0600

    :sparkles: Add get users endpoint

--- src/users/repositories/users.repository.ts
+++ src/users/repositories/users.repository.ts
@@ -1,4 +1,7 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { PageDto } from '@core/dtos/page.dto';
+import { UserFilterDto, sortOptionsMap } from '@users/dtos/user-filter.dto';
+import { getOrderBy } from '@core/utils/sort.util';
 import { User } from '../entities/users.entity';
 
 @EntityRepository(User)
@@ -21,5 +24,68 @@ export class UserRepository extends Repository<User> {
         code,
       },
     });
+  }
+
+  getAllUsers(pageDto: PageDto, userFilterDto: UserFilterDto): Promise<[User[], number]> {
+    const { page, perPage } = pageDto;
+    const {
+      sort,
+      username,
+      firstname,
+      lastname,
+      email,
+      active,
+      credentials,
+      role,
+      createdAtStart,
+      createdAtEnd,
+    } = userFilterDto;
+    const query = this.createQueryBuilder('user')
+      .leftJoinAndSelect('user.roles', 'role')
+      .andWhere('user.deletedAt is null')
+      .andWhere(`user.active is ${active}`)
+      .take(perPage)
+      .skip((page - 1) * perPage);
+
+    if (sort) {
+      const order = getOrderBy(sort, sortOptionsMap);
+      query.orderBy(order);
+    } else {
+      query.orderBy({ 'user.id': 'DESC' });
+    }
+
+    if (credentials) {
+      query.andWhere(`user.password is null`);
+    }
+
+    if (username) {
+      query.andWhere(`user.username ILIKE '%${username}%'`);
+    }
+
+    if (firstname) {
+      query.andWhere(`user.firstname ILIKE '%${firstname}%'`);
+    }
+
+    if (lastname) {
+      query.andWhere(`user.lastname ILIKE '%${lastname}%'`);
+    }
+
+    if (email) {
+      query.andWhere(`user.email ILIKE '%${email}%'`);
+    }
+
+    if (role) {
+      query.andWhere(`role.id = ${role}`);
+    }
+
+    if (createdAtStart) {
+      query.andWhere(`user.createdAt >= '${createdAtStart}'`);
+    }
+
+    if (createdAtEnd) {
+      query.andWhere(`user.createdAt <= '${createdAtEnd}'`);
+    }
+
+    return query.getManyAndCount();
   }
 }
