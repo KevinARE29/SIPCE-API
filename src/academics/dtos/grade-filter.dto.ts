eb4b8f32438dec828d2b05a03b9961dac7c7dfcb
Fernando <fernandorivasarevalo27@gmail.com>
2020-07-15 00:43:02 -0600

    :sparkles: add list and deactivate grades

---
+++ src/academics/dtos/grade-filter.dto.ts
@@ -0,0 +1,24 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, Validate, IsString, IsBooleanString } from 'class-validator';
+import { SortOptionsValidator } from '@core/validators/sort-options.validator';
+import { validator } from '@core/messages/validator.message';
+import { getSortOptions, getSortOptionsMap } from '@core/utils/sort.util';
+
+const sortOptions = getSortOptions('name');
+export const sortOptionsMap = getSortOptionsMap('grade', sortOptions);
+
+export class GradeFilterDto {
+  @ApiPropertyOptional({ enum: sortOptions })
+  @IsOptional()
+  @Validate(SortOptionsValidator, sortOptions)
+  sort?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: validator.isString })
+  name?: string;
+
+  @IsOptional()
+  @IsBooleanString({ message: validator.isBoolean })
+  active?: boolean;
+}
--- src/academics/repositories/grade.repository.ts
+++ src/academics/repositories/grade.repository.ts
@@ -1,5 +1,43 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Grade } from '@academics/entities/grade.entity';
+import { NotFoundException } from '@nestjs/common';
+import { PageDto } from '@core/dtos/page.dto';
+import { GradeFilterDto, sortOptionsMap } from '@academics/dtos/grade-filter.dto';
+import { getOrderBy } from '@core/utils/sort.util';
 
 @EntityRepository(Grade)
-export class GradeRepository extends Repository<Grade> {}
+export class GradeRepository extends Repository<Grade> {
+  getAllGrades(pageDto: PageDto, gradeFilterDto: GradeFilterDto): Promise<[Grade[], number]> {
+    const { page, perPage } = pageDto;
+    const { sort, name, active } = gradeFilterDto;
+    const query = this.createQueryBuilder('grade')
+      // .andWhere('grade.deletedAt is null')
+      .take(perPage)
+      .skip((page - 1) * perPage);
+
+    if (sort) {
+      const order = getOrderBy(sort, sortOptionsMap);
+      query.orderBy(order);
+    } else {
+      query.orderBy({ 'grade.id': 'DESC' });
+    }
+
+    if (name) {
+      query.andWhere(`grade."name" ILIKE '%${name}%'`);
+    }
+
+    if (active) {
+      query.andWhere(`grade.active is ${active}`);
+    }
+
+    return query.getManyAndCount();
+  }
+
+  async getGradeByIdOrThrow(gradeId: number): Promise<Grade> {
+    const grade = await this.findOne(gradeId);
+    if (!grade) {
+      throw new NotFoundException('Grado no encontrado');
+    }
+    return grade;
+  }
+}
