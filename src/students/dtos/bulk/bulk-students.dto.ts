ab2c907c09179f5d67e9260573a7d4438cd2fdb3
Kevin Escoto <kevin29_kare@hotmail.es>
2020-07-22 22:47:46 -0600

    :sparkles: Fix bulk user dto

--- src/students/dtos/bulk/bulk-students.dto.ts
+++ src/students/dtos/bulk/bulk-students.dto.ts
@@ -1,5 +1,5 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, ValidateNested } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsArray, ValidateNested, IsBoolean, IsOptional } from 'class-validator';
 import { validator } from '@core/messages/validator.message';
 import { Type } from 'class-transformer/decorators';
 import { IsId } from '@core/decorators/id.decorator';
@@ -8,6 +8,11 @@ import { StudentDto } from './student.dto';
 export class BulkStudentsDto {
   @IsId()
   shiftId!: number;
+
+  @ApiPropertyOptional({ type: Boolean })
+  @IsOptional()
+  @IsBoolean({ message: validator.isBoolean })
+  currentYear?: boolean;
 
   @ApiProperty({ type: [StudentDto] })
   @IsArray({ message: validator.isArray })
