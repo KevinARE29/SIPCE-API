505a7ddd739eb803304e53e7a4fa6e9c95ba3d54
Kevin Escoto <kevinare295@gmail.com>
2020-09-19 19:23:52 -0600

    :recycle: Refactor academics module

---
+++ src/academics/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from './close-school-year.controller';
+export * from './cycle.controller';
+export * from './grade.controller';
+export * from './period.controller';
+export * from './school-year.controller';
+export * from './section.controller';
+export * from './shift.controller';
---
+++ src/academics/services/index.ts
@@ -0,0 +1,7 @@
+export * from './close-school-year.service';
+export * from './cycle.service';
+export * from './grade.service';
+export * from './period.service';
+export * from './school-year.service';
+export * from './section.service';
+export * from './shift.service';
