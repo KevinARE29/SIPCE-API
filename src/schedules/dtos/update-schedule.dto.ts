42e2f813bc652f9bb4a25a132f54bec55ab06ce1
Kevin Escoto <kevinare295@gmail.com>
2020-09-20 00:52:45 -0600

    :recycle: Add mapped types to update schedule dto

--- src/schedules/dtos/update-schedule.dto.ts
+++ src/schedules/dtos/update-schedule.dto.ts
@@ -1,27 +1,4 @@
-import { IsArray, IsOptional, IsEnum, IsObject } from 'class-validator';
-import { validator } from '@core/messages/validator.message';
-import { ApiProperty } from '@nestjs/swagger';
-import { EnumEventType, schedulesKeys, TSchedule } from '@schedules/constants/schedule.costants';
-import { IsId } from '@core/decorators/id.decorator';
+import { PartialType } from '@nestjs/swagger';
+import { CreateScheduleDto } from './create-schedule.dto';
 
-export class UpdateScheduleDto {
-  @ApiProperty({ type: String })
-  @IsOptional()
-  @IsEnum(EnumEventType, {
-    message: `EnumEventType: Debe ser uno de los siguientes valores: ${schedulesKeys}`,
-  })
-  readonly eventType?: TSchedule;
-
-  @IsOptional()
-  @IsObject()
-  readonly jsonData?: Record<string, any>;
-
-  @IsOptional()
-  @IsArray({ message: validator.isArray })
-  @IsId({ each: true })
-  readonly participantIds?: number[];
-
-  @IsOptional()
-  @IsId()
-  readonly studentId?: number;
-}
+export class UpdateScheduleDto extends PartialType(CreateScheduleDto) {}
