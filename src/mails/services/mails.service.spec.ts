8d3634d569103809bf0c6aa52926c87bb2927cd5
Kevin Escoto <kevinare295@gmail.com>
2020-10-03 03:42:26 -0600

    :white_check_mark: Update mail service tests

--- src/mails/services/mails.service.spec.ts
+++ src/mails/services/mails.service.spec.ts
@@ -1,39 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ConfigService } from '@nestjs/config';
-import { send } from '@sendgrid/mail';
+import { MailerService } from '@nestjs-modules/mailer';
 import { MailsService } from './mails.service';
 
-jest.mock('@sendgrid/mail');
+jest.mock('@nestjs-modules/mailer');
 
 const mockEmail = {
   to: 'fake@email.com',
-  from: 'fake@email.com',
-  templateId: '',
-  dynamicTemplateData: {},
+  template: 'mock-template',
+  subject: 'Mock Subject',
+  context: {},
 };
 
 describe('Mails Service', () => {
   let mailsService: MailsService;
-  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MailsService, ConfigService],
+      providers: [MailsService, MailerService],
     }).compile();
 
     mailsService = module.get(MailsService);
-    configService = module.get(ConfigService);
   });
 
   it('Should be defined', () => {
     expect(mailsService).toBeDefined();
-    expect(configService).toBeDefined();
   });
 
   describe('Send Reset Password Email', () => {
     it('Should send a reset password email', async () => {
-      mailsService.sendEmail(mockEmail);
-      expect(send).toHaveBeenCalledTimes(1);
+      expect(await mailsService.sendEmail(mockEmail)).toBeUndefined();
     });
   });
 });
