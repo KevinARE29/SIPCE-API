881a04908da714dfa48175e41a802a9379f4f551
Kevin Escoto <kevinare295@gmail.com>
2020-10-24 21:22:47 -0600

    :sparkles: Add request gateway

--- src/counseling/counseling.module.ts
+++ src/counseling/counseling.module.ts
@@ -5,13 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StudentModule } from '@students/students.module';
 import { CounselingRequestsController } from './controllers/counseling-requests.controller';
 import { MeRequestsController } from './controllers/me-requests.controller';
+import { RequestGateway } from './gateways/request.gateway';
 import { RequestRepository } from './repositories/request.repository';
 import { RequestService } from './services/request.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RequestRepository]), MailsModule, StudentModule, AcademicsModule],
   controllers: [CounselingRequestsController, MeRequestsController],
-  providers: [RequestService],
+  providers: [RequestService, RequestGateway],
   exports: [TypeOrmModule],
 })
 export class CounselingModule {}
