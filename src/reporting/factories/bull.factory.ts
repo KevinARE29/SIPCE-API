e3daf8cec0b66ddf7edeeb6c3a828ed8905b7689
Kevin Escoto <kevinare295@gmail.com>
2020-11-09 03:28:15 -0600

    :wrench: Update bull factory

--- src/reporting/factories/bull.factory.ts
+++ src/reporting/factories/bull.factory.ts
@@ -4,5 +4,6 @@ export const bullFactory = async (configService: ConfigService) => ({
   redis: {
     host: configService.get('REDIS_HOST'),
     port: +configService.get('REDIS_PORT'),
+    password: configService.get('REDIS_PASSWORD'),
   },
 });
