1f093a3f426861f5b17ae6af4456d9dc8a682c5c
Kevin Escoto <kevinare295@gmail.com>
2020-11-08 21:54:32 -0600

    :sparkles: Add bull factory

---
+++ src/reporting/factories/bull.factory.ts
@@ -0,0 +1,8 @@
+import { ConfigService } from '@nestjs/config';
+
+export const bullFactory = async (configService: ConfigService) => ({
+  redis: {
+    host: configService.get('REDIS_HOST'),
+    port: +configService.get('REDIS_PORT'),
+  },
+});
