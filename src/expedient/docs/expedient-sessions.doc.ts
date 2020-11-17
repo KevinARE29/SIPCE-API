f421e2f57363aea3bc6a319615c48af0a824cb76
KevinARE29 <kevleiva20@gmail.com>
2020-11-16 22:33:03 -0600

    :fire: Remove unused file

--- src/expedient/docs/expedient-sessions.doc.ts
+++
@@ -1,9 +0,0 @@
-import { Expose, Type } from 'class-transformer';
-import { Session } from '@expedient/docs/session.doc';
-import { Expedient } from './expedient.doc';
-
-export class ExpedientSessions extends Expedient {
-  @Expose()
-  @Type(() => Session)
-  sessions!: Session[];
-}
