3b7f91e47c9bffd230b3e4335ac6e866f9bfceb8
KevinARE29 <kevleiva20@gmail.com>
2020-11-19 21:43:39 -0600

    :memo: Add endpoint documentation

--- src/expedient/docs/intervention-program-response.doc.ts
+++ src/expedient/docs/intervention-program-response.doc.ts
@@ -1,11 +1,8 @@
 import { IApiResponse } from '@core/interfaces/api-response.interface';
 import { ApiProperty } from '@nestjs/swagger';
-import { Pagination } from '@core/docs/pagination.doc';
 import { InterventionProgram } from './intervention-program.doc';
 
-export class InterventionProgramResponse implements IApiResponse<InterventionProgram[]> {
-  @ApiProperty({ type: [InterventionProgram] })
-  data!: InterventionProgram[];
-
-  pagination!: Pagination;
+export class InterventionProgramResponse implements IApiResponse<InterventionProgram> {
+  @ApiProperty({ type: InterventionProgram })
+  data!: InterventionProgram;
 }
