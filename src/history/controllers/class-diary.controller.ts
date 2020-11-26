ca2b780ff7ff8a686ebfa063574c948bf9d8d749
KevinARE29 <kevleiva20@gmail.com>
2020-11-26 13:09:43 -0600

    :sparkles: Add feature to update annotation

--- src/history/controllers/class-diary.controller.ts
+++ src/history/controllers/class-diary.controller.ts
@@ -1,5 +1,5 @@
 import { ContentTypeGuard } from '@core/guards/content-type.guard';
-import { Controller, UseGuards, Post, Param, Body } from '@nestjs/common';
+import { Controller, UseGuards, Post, Param, Body, Patch } from '@nestjs/common';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { ClassDiaryService } from '@history/services/class-diary.service';
 import { User } from '@users/decorators/user.decorator';
@@ -8,6 +8,8 @@ import { StudentHistoryIdsDto } from '@history/dtos/student-history-ids.dto';
 import { CreateAnnotationDto } from '@history/dtos/create-annotation.dto';
 import { Auth } from '@auth/decorators/auth.decorator';
 import { AnnotationResponse } from '@history/docs/annotation-response.doc';
+import { HistoryAnnotationIdsDto } from '@history/dtos/history-annotation-ids.dto';
+import { UpdateAnnotationDto } from '@history/dtos/update-annotation.dto';
 
 @ApiTags('Class Diary Endpoints (Annotations)')
 @UseGuards(ContentTypeGuard)
@@ -27,5 +29,19 @@ export class ClassDiaryController {
     @Body() createAnnotationDto: CreateAnnotationDto,
   ): Promise<AnnotationResponse> {
     return this.classDiaryService.createClassDiaryAnnotation(id, studentHistoryIdsDto, createAnnotationDto);
+  }
+
+  @ApiOperation({
+    summary: 'Actualizar una anotación en un historial académico y conductual',
+    description: 'Use este endpoint para actualizar una anotación en un historial académico y conductual',
+  })
+  @Auth('update_class_diary')
+  @Patch('annotations/:annotationId')
+  updateAnnotation(
+    @User() { id }: IAuthenticatedUser,
+    @Param() historyAnnotationIdsDto: HistoryAnnotationIdsDto,
+    @Body() updateAnnotationDto: UpdateAnnotationDto,
+  ): Promise<AnnotationResponse> {
+    return this.classDiaryService.updateClassDiaryAnnotation(id, historyAnnotationIdsDto, updateAnnotationDto);
   }
 }
