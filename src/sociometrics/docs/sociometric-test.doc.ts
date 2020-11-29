d2b682941d93934501d06520b9995005e4706555
Kevin Escoto <kevinare295@gmail.com>
2020-11-29 00:26:21 -0600

    :pencil: Update sociometric test documentation

--- src/sociometrics/docs/sociometric-test.doc.ts
+++ src/sociometrics/docs/sociometric-test.doc.ts
@@ -2,6 +2,9 @@ import { Grade } from '@academics/docs/grade.doc';
 import { Section } from '@academics/docs/section.doc';
 import { Shift } from '@academics/docs/shift.doc';
 import { Expose, Type } from 'class-transformer';
+import { Preset } from './preset.doc';
+import { QuestionBank } from './question-bank.doc';
+import { SociometricTestStudent } from './sociometric-test-student.doc';
 
 export class SociometricTest {
   @Expose()
@@ -26,8 +29,23 @@ export class SociometricTest {
   status!: string;
 
   @Expose()
+  answersPerQuestion!: string;
+
+  @Expose()
   totalStudents!: number;
 
   @Expose()
   completedStudents!: number;
+
+  @Expose()
+  @Type(() => QuestionBank)
+  questionBank!: QuestionBank;
+
+  @Expose()
+  @Type(() => Preset)
+  presets!: Preset[];
+
+  @Expose()
+  @Type(() => SociometricTestStudent)
+  students!: SociometricTestStudent[];
 }
