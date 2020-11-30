6f9e079c1458b65eba372facd6053a145f726825
Kevin Escoto <kevinare295@gmail.com>
2020-11-30 01:07:21 -0600

    :sparkles: Add repositories for answers and questions

--- src/sociometrics/repositories/question.repository.ts
+++ src/sociometrics/repositories/question.repository.ts
@@ -1,5 +1,14 @@
+import { NotFoundException } from '@nestjs/common';
 import { Question } from '@sociometrics/entities/quetion.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Question)
-export class QuestionRepository extends Repository<Question> {}
+export class QuestionRepository extends Repository<Question> {
+  async findByIdOrThrow(id: number): Promise<Question> {
+    const question = await this.findOne(id);
+    if (!question) {
+      throw new NotFoundException(`Pregunta con id ${id} no encontrada`);
+    }
+    return question;
+  }
+}
---
+++ src/sociometrics/repositories/sociometric-test-detail.repository.ts
@@ -0,0 +1,26 @@
+import { EntityRepository, Repository } from 'typeorm';
+import { SociometricTestDetail } from '@sociometrics/entities/sociometric-test-detail.entity';
+
+@EntityRepository(SociometricTestDetail)
+export class SociometricTestDetailRepository extends Repository<SociometricTestDetail> {
+  async findOrCreate(sociometricTestId: number, studentId: number): Promise<SociometricTestDetail> {
+    const sociometricTestDetail = await this.createQueryBuilder('sociometricTestDetail')
+      .leftJoinAndSelect('sociometricTestDetail.sociometricTest', 'sociometricTest')
+      .leftJoinAndSelect('sociometricTestDetail.student', 'student')
+      .leftJoinAndSelect('sociometricTestDetail.answers', 'answer')
+      .leftJoinAndSelect('answer.question', 'question')
+      .leftJoinAndSelect('answer.student', 'selectedStudent')
+      .leftJoinAndSelect('sociometricTest.sectionDetail', 'sectionDetail')
+      .leftJoinAndSelect('sectionDetail.section', 'section')
+      .leftJoinAndSelect('sectionDetail.gradeDetail', 'gradeDetail')
+      .leftJoinAndSelect('gradeDetail.grade', 'grade')
+      .andWhere(`sociometricTest.id = ${sociometricTestId}`)
+      .andWhere(`student.id = ${studentId}`)
+      .getOne();
+
+    if (!sociometricTestDetail) {
+      return this.save({ student: { id: studentId }, sociometricTest: { id: sociometricTestId } });
+    }
+    return sociometricTestDetail;
+  }
+}
