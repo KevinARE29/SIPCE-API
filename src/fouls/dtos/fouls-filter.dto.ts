d320f4ec2f919b596086af6b58e264d888c62fed
fernando27Rivas <fernandorivasarevalo27@gmail.com>
2020-12-01 18:13:10 -0600

    :fire: corrections for controler and filter

--- src/fouls/dtos/fouls-filter.dto.ts
+++ src/fouls/dtos/fouls-filter.dto.ts
@@ -2,7 +2,7 @@ import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional, Validate, IsEnum, IsIn, IsString } from 'class-validator';
 import { SortOptionsValidator } from '@core/validators/sort-options.validator';
 import { getSortOptionsv2 } from '@core/utils/sort.util';
-import { EnumFoulsType, TFouls } from '@fouls/constants/fouls.constants';
+import { EnumFoulsType, foulsKeys, TFouls } from '@fouls/constants/fouls.constants';
 import { validator } from '@core/messages/validator.message';
 
 export const [sortOptions, sortOptionsMap] = getSortOptionsv2(['foulsType', 'numeral'], 'fouls');
@@ -14,7 +14,7 @@ export class FoulsFilterDto {
 
   @ApiPropertyOptional({ type: String })
   @IsOptional()
-  @IsEnum(EnumFoulsType)
+  @IsEnum(EnumFoulsType, { message: `fouls_type: Debe ser uno de los siguientes valores: ${foulsKeys}` })
   readonly foulsType?: TFouls;
 
   @IsOptional()
