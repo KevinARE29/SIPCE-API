bb5aebecd13c82786407b3e2b230990c83fe7288
fernando27Rivas <fernandorivasarevalo27@gmail.com>
2020-12-08 03:58:34 -0600

    :fire:solve fix

--- migrations/1607415292614-add-access-log-prune-sp-doc.migration.ts
+++ migrations/1607415292614-add-access-log-prune-sp-doc.migration.ts
@@ -3,12 +3,12 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AddAccesLogPruneSPDocMigration1607415292614 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `comment on procedure "acces_log_prune_sp" is 'Procedimiento almacenado que elimina los registros de accesos de los usuarios al sistema informático, se ejecuta una vez al año';`,
+      `comment on procedure "access_log_prune_sp" is 'Procedimiento almacenado que elimina los registros de accesos de los usuarios al sistema informático, se ejecuta una vez al año';`,
       undefined,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`comment on procedure "acces_log_prune_sp" is '';`, undefined);
+    await queryRunner.query(`comment on procedure "access_log_prune_sp" is '';`, undefined);
   }
 }
